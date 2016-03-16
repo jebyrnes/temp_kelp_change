9e1a3fa003210f735a9e7d418fde8d0b0c04a5ba
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2016-03-16 17:18:21 -0400

    Included focalUnit error modification from Dan

--- R/05_raw_rethinking_analysis.R
+++ R/05_raw_rethinking_analysis.R
@@ -19,7 +19,7 @@ kelp_data <- read.csv("../derived_data/raw_data_merged.csv", stringsAsFactors=FA
   
 
 kelp_data_to_fit <- kelp_data %>%
-  select(y, Year_c, Group, Site, max_waves_c, max_tempC_anomoly, abs_lat)
+  select(y, Year_c, Group, Study, Site, max_waves_c, max_tempC_anomoly, abs_lat, focalUnit)
 
 kelp_mod <- alist(
   #likelihood
@@ -33,7 +33,7 @@ kelp_mod <- alist(
     (beta_slope_temp_waves[Site] + beta_mu_slope_temp_waves[Group])*max_tempC_anomoly*max_waves_c
     ,#expectation equation
   
-  sd_loc <- sd_e[Site],
+  sd_loc <- sd_e[Study],
   
   #priors
   beta_mu_int[Group] ~ dnorm(0,10),
@@ -48,21 +48,26 @@ kelp_mod <- alist(
   
   Rho_Site ~ dlkjcorr(2.0), # vague priors for the ranef corr matrix
   
-  sd_e[Site] ~ dcauchy(0,1) #prior for study level errors
+#  sd_e[Study] ~ dcauchy(0,1) #prior for study level errors
+  
+  sd_e[Study] ~ dcauchy(0,A), #prior for study level errors
+  A[focalUnit] ~ dunif(0.000001, 3) #hyperprior for method-based error
 )
 
 
 kelp_fit <- map2stan(kelp_mod, data=kelp_data_to_fit, chains=4, cores=4, 
-                     constraints=list(sd_e = "lower=0"), 
-                     start=list(sd_e = rep(1,length(unique(kelp_data$Site)))))
+                     constraints=list(sd_e = "lower=0.000001, upper= 5"), 
+                     start=list(sd_e = rep(1,length(unique(kelp_data$Study))),
+                                A = rep(1,4)))
 
 save(kelp_fit, file="../HLM_output/rethinking_kelp_fit_ecoregion.Rdata")
 
 
 kelp_fit_realm <- map2stan(kelp_mod, data=kelp_data_to_fit %>% mutate(Group = kelp_data$REALM),
                               chains=4, cores=4,
-                              constraints=list(sd_e = "lower=0"), 
-                              start=list(sd_e = rep(1,length(unique(kelp_data$Site)))))
+                           constraints=list(sd_e = "lower=0.000001, upper= 5"), 
+                           start=list(sd_e = rep(1,length(unique(kelp_data$Study))),
+                                      A = rep(1,4)))
 
 save(kelp_fit_realm, file="rethinking_kelp_fit_realm.Rdata")
 
@@ -70,8 +75,9 @@ save(kelp_fit_realm, file="rethinking_kelp_fit_realm.Rdata")
 
 kelp_fit_province <- map2stan(kelp_mod, data=kelp_data_to_fit %>% mutate(Group = kelp_data$PROVINCE),
                               chains=4, cores=4,
-                              constraints=list(sd_e = "lower=0"), 
-                              start=list(sd_e = rep(1,length(unique(kelp_data$Site)))))
+                              constraints=list(sd_e = "lower=0.000001, upper= 5"), 
+                              start=list(sd_e = rep(1,length(unique(kelp_data$Study))),
+                                         A = rep(1,4)))
 
 save(kelp_fit_province, file="rethinking_kelp_fit_province.Rdata")
 
@@ -100,7 +106,7 @@ kelp_lat_global_mod <- alist(
     (beta_slope_temp_waves_lat[Site] + beta_mu_slope_temp_waves_lat)*max_tempC_anomoly*max_waves_c * abs_lat 
   ,#expectation equation
   
-  sd_loc <- sd_e[Site],
+  sd_loc <- sd_e[Study],
   
   #priors
   beta_mu_int ~ dnorm(0,10),
@@ -123,14 +129,17 @@ kelp_lat_global_mod <- alist(
   
   Rho_Site ~ dlkjcorr(2.0), # vague priors for the ranef corr matrix
   
-  sd_e[Site] ~ dcauchy(0,1) #prior for study level errors
+#  sd_e[Site] ~ dcauchy(0,1) #prior for study level errors
+  sd_e[Study] ~ dcauchy(0,A), #prior for study level errors
+  A[focalUnit] ~ dunif(0.000001, 3) #hyperprior for method-based error
 )
 
 
 kelp_lat_global_fit <- map2stan(kelp_mod, data=kelp_data_to_fit %>% mutate(Group = "World"), 
                            chains=4, cores=4,
-                           constraints=list(sd_e = "lower=0"), 
-                           start=list(sd_e = rep(1,length(unique(kelp_data$Site)))))
+                           constraints=list(sd_e = "lower=0.000001, upper= 5"), 
+                           start=list(sd_e = rep(1,length(unique(kelp_data$Study))),
+                                      A = rep(1,4)))
 
 save(kelp_lat_global_fit, file="../HLM_output/rethinking_kelp_fit_lat_global.Rdata")
 
