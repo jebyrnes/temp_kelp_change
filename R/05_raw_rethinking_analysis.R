8ff489380e8aa254f4545bbb11de28fe569fffa9
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2016-03-16 10:30:37 -0400

    Modified model to match Dan's trend model

--- R/05_raw_rethinking_analysis.R
+++ R/05_raw_rethinking_analysis.R
@@ -2,8 +2,8 @@ library(rethinking)
 library(dplyr)
 library(ggplot2)
 
-#rstan_options(auto_write = TRUE)
-#options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
 
 kelp_data <- read.csv("../derived_data/raw_data_merged.csv", stringsAsFactors=FALSE) %>%
   dplyr::rename(Group = ECOREGION) %>%
@@ -15,11 +15,12 @@ kelp_data <- read.csv("../derived_data/raw_data_merged.csv", stringsAsFactors=FA
   dplyr::mutate(Site = StudySite) %>%
   dplyr::mutate(y = stdByECOREGION+0.001) %>%
   dplyr::mutate(max_waves_c = max_waves-mean(max_waves)) %>%
-  dplyr::mutate(mean_tempC_c = mean_tempC-mean(mean_tempC))
+  dplyr::mutate(mean_tempC_c = mean_tempC-mean(mean_tempC)) %>%
+  dplyr::mutate(abs_lat = abs(Latitude))
   
 
 kelp_data_to_fit <- kelp_data %>%
-  select(y, Year_c, Group, Site, max_waves_c, mean_tempC_c)
+  select(y, Year_c, Group, Site, max_waves_c, mean_tempC_c, abs_lat)
 
 kelp_mod <- alist(
   #likelihood
@@ -56,15 +57,7 @@ kelp_fit <- map2stan(kelp_mod, data=kelp_data_to_fit, chains=4, cores=4,
                      constraints=list(sd_e = "lower=0"), 
                      start=list(sd_e = rep(1,length(unique(kelp_data$Site)))))
 
-save(kelp_fit, file="rethinking_kelp_fit_ecoregion.Rdata")
-
-####Global model
-kelp_fit_world <- map2stan(kelp_mod, data=kelp_data_to_fit %>% mutate(Group = "World"), 
-                           chains=4, cores=4,
-                           constraints=list(sd_e = "lower=0"), 
-                           start=list(sd_e = rep(1,length(unique(kelp_data$Site)))))
-
-save(kelp_fit_world, file="rethinking_kelp_fit_world.Rdata")
+save(kelp_fit, file="../HLM_output/rethinking_kelp_fit_ecoregion.Rdata")
 
 
 kelp_fit_realm <- map2stan(kelp_mod, data=kelp_data_to_fit %>% mutate(Group = kelp_data$REALM),
@@ -87,5 +80,58 @@ save(kelp_fit_province, file="rethinking_kelp_fit_province.Rdata")
 
 precis(kelp_fit_world, depth=2, pars=c("beta_mu_int", "beta_mu_slope_year",
                                        "beta_mu_slope_year", "beta_mu_slope_temp",
-                                       "beta_mu_slope_waves", "beta_mu_slope_temp_waves"),
-       prob=95)
+                                       "beta_mu_slope_waves", "beta_mu_slope_temp_waves"))
+
+
+
+kelp_lat_global_mod <- alist(
+  #likelihood
+  y ~ lognormal(y_loc, sd_loc), # likelihood
+  
+  #model
+  y_loc <- beta_mu_int + beta_int[Site] + 
+    (beta_slope_year[Site] + beta_mu_slope_year)*Year_c +
+    (beta_slope_temp[Site] + beta_mu_slope_temp)*mean_tempC_c + 
+    (beta_slope_waves[Site] + beta_mu_slope_waves)*max_waves_c +
+    (beta_slope_lat[Site] + beta_mu_slope_lat)*abs_lat +
+    (beta_slope_temp_waves[Site] + beta_mu_slope_temp_waves)*mean_tempC_c*max_waves_c +
+    (beta_slope_temp_lat[Site] + beta_mu_slope_temp_lat)*mean_tempC_c*abs_lat +
+    (beta_slope_lat_waves[Site] + beta_mu_slope_lat_waves)*abs_lat*max_waves_c +
+    (beta_slope_temp_waves[Site] + beta_mu_slope_temp_waves)*mean_tempC_c*max_waves_c +
+    (beta_slope_temp_waves_lat[Site] + beta_mu_slope_temp_waves_lat)*mean_tempC_c*max_waves_c * abs_lat 
+  ,#expectation equation
+  
+  sd_loc <- sd_e[Site],
+  
+  #priors
+  beta_mu_int ~ dnorm(0,10),
+  beta_mu_slope_year ~ dnorm(0,10),
+  beta_mu_slope_temp ~ dnorm(0,10),
+  beta_mu_slope_waves ~ dnorm(0,10),
+  beta_mu_slope_lat ~ dnorm(0,10),
+  beta_mu_slope_temp_waves ~ dnorm(0,10),
+  beta_mu_slope_lat_waves ~ dnorm(0,10),
+  beta_mu_slope_temp_waves ~ dnorm(0,10),
+  beta_mu_slope_temp_waves_lat ~ dnorm(0,10),
+  
+  c(beta_int,beta_slope_year, beta_slope_temp, 
+    beta_slope_waves, beta_slope_lat,
+    beta_slope_temp_lat, beta_slope_lat_waves,
+    beta_slope_temp_waves,beta_slope_temp_waves_lat
+    )[Site] ~ dmvnormNC( sigma_Site , Rho_Site ),
+  
+  sigma_Site ~ dcauchy(0,2.5), # vague priors for the group SD
+  
+  Rho_Site ~ dlkjcorr(2.0), # vague priors for the ranef corr matrix
+  
+  sd_e[Site] ~ dcauchy(0,1) #prior for study level errors
+)
+
+
+kelp_lat_global_fit <- map2stan(kelp_mod, data=kelp_data_to_fit %>% mutate(Group = "World"), 
+                           chains=4, cores=4,
+                           constraints=list(sd_e = "lower=0"), 
+                           start=list(sd_e = rep(1,length(unique(kelp_data$Site)))))
+
+save(kelp_lat_global_fit, file="../HLM_output/rethinking_kelp_fit_lat_global.Rdata")
+
