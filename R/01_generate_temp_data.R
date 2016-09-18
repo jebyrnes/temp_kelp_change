da0d5d36a5401a7615e30a1cbe0bbc5b2087d4f3
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2016-09-18 14:25:12 -0400

    Updated temp script to use nearest non-na cell

--- R/01_generate_temp_data.R
+++ R/01_generate_temp_data.R
@@ -17,89 +17,65 @@ unique_lat_long <- raw_data %>%
 ull_points <- SpatialPoints(cbind(unique_lat_long$Longitude, unique_lat_long$Latitude),
                             proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 
-
 ###### 3) Load and rasterize HADSST data set from 1950 - 2013
 hadsst <- raster::brick("~/Dropbox/src/HADSST/HadISST_sst.nc")
 #hadsst <- raster::brick("~/Dropbox/src/HADSST/HadISST_ice.nc")
 raster::NAvalue(hadsst) <- -1000 #make sure we don't have any super small NAs
 yearIDx <- which(chron::years(hadsst@z$Date) %in% 1950:2013)
+#hadsst_subset <- stack(hadsst)
 hadsst_subset <- raster::subset(hadsst, names(hadsst)[yearIDx]) 
 
-###### 4) Extract data at only those lat/longs used
-#area_raster <- raster::area(hadsst) #gets area in sq km
-#radii <- sqrt(raster::extract(area_raster, cbind(unique_lat_long$Longitude, unique_lat_long$Latitude)))/2*1000
-all_cells <- cellFromXY(hadsst, ull_points)
-unique_lat_long$cell <- all_cells
+###### 4) Get the unique non-NA cells
+#Based on answer from
+# from http://stackoverflow.com/questions/27562076/if-raster-value-na-search-and-extract-the-nearest-non-na-pixel/39539718#39539718
+r <- hadsst_subset@layers[[length(hadsst_subset@layers)]]
+r <- readAll(r)
 
-unique_cells <- unique(all_cells)
+non_na_cells <- 
+  simplify2array(apply(as.data.frame(ull_points), 1, function(xy)
+    which.min(replace(distanceFromPoints(r, xy), is.na(r), NA))[1]))
+
+unique_lat_long$cell <- cellFromXY(hadsst_subset, ull_points)
+unique_lat_long$non_na_cell <- non_na_cells
+
+###### 4) Extract data at only those lat/longs used
+
+unique_cells <- unique(non_na_cells)
 
 hadsst_kelp_vals <- raster::extract(hadsst_subset, 
                                     unique_cells) 
 
-unique_cell_frame <- data.frame(cell = unique_cells, hadsst_kelp_vals)
+unique_cell_frame <- data.frame(non_na_cell = unique_cells, hadsst_kelp_vals)
 
-#deal with NAs
-
-###### Deal with NAs
-naCells_idx <- which(is.na(unique_cell_frame[,2]))
-
-if(length(naCells_idx)>0){
-  
-  adj_cells <- data.frame(adjacent(x = hadsst, 
-                                   cells = unique_cells[naCells_idx], 
-                                   directions = 8)) %>%
-    mutate(cell_num = match(to, unique_cells))
-  
-  unknown_cells <- unique(adj_cells$to[which(is.na(adj_cells$cell_num))])
-  
-  print("Extracting NA cells")
-  #presuming there aren't many
-  if(length(unknown_cells)>0){
-    unk_vals <- cbind(cell = unknown_cells, raster::extract(hadsst_subset, 
-                                unknown_cells))
-    
-    unique_cell_frame <- rbind(unique_cell_frame,  unk_vals)
-  }
-  
-  print("Finished extracting NA cells")
-  
-  #Now merge adj_cells with hadsst_extracted on to column
-  adj_cells <- left_join(adj_cells, unique_cell_frame, by=c("to" = "cell"))
-  
-  #take averages on from column
-  adj_cells <- adj_cells %>% 
-    dplyr::select(-to, -cell_num) %>%
-    split(.$from) %>%
-    map_df(.f=function(x) data.frame(t(colMeans(x, na.rm=T)))) %>%
-    ungroup() %>%
-    dplyr::rename(cell = from)
-  
-  #fill in NA rows in hadsst_extracted
-  htIDX <- match(adj_cells$cell, unique_cell_frame$cell)
-  unique_cell_frame[htIDX,] <- adj_cells
-  
-  #cleanup hadsst_extracted
-  if(length(unknown_cells)>0)
-    unique_cell_frame <- unique_cell_frame[-c(length((unique_cells)+1):nrow(unique_cell_frame)),]
-  
-}
+###### 5) Join data to unique cells
 
 ht <- left_join(unique_lat_long, unique_cell_frame) %>%
-  dplyr::select(-cell, -len)
+  dplyr::select(-len)
 
 hadsst_kelp <- ht %>%
-  gather(DateName, tempC, -Latitude, -Longitude) %>%
+  gather(DateName, tempC, -Latitude, -Longitude, -cell, -non_na_cell) %>%
   mutate(DateName = gsub("X", "", as.character(DateName))) %>%
-  mutate(Year = year(parse_date_time(DateName, orders="ymd")))# %>%
-#  mutate(tempC=ifelse(tempC == -1000, NA, tempC))
-  
+  mutate(Year = year(parse_date_time(DateName, orders="ymd"))) %>%
+  arrange(Latitude, Longitude)
+
+#Make sure that no lat/longs have NAs all through the record
+#A few is fine, as the nearest cell with a value might be missing
+#for some dates - should not be a large impact on results
+as.data.frame(hadsst_kelp[which(is.na(hadsst_kelp$tempC)),])
+
+#make sure there are no stupidly high values
+as.data.frame(hadsst_kelp[which(hadsst_kelp$tempC>100),])
+
 ######FOR ICE
 #hadsst_kelp <- hadsst_kelp %>%
 #  rename(ice_conc = tempC)
 #write.csv(hadsst_kelp, "../derived_data/hadice_at_latlongs.csv", row.names=F)
 
 ###### 5) Write out temp kelp data as an intermediate step
-write.csv(hadsst_kelp, "../derived_data/hadsst_at_latlongs.csv", row.names=F)
+
+hadsst_kelp_clean <- hadsst_kelp %>%
+  select(-cell, -non_na_cell)
+write.csv(hadsst_kelp_clean, "../derived_data/hadsst_at_latlongs.csv", row.names=F)
 
 
 
