28a1e7eec36b707bd4cee7ea78efb31a5131d5ac
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2016-11-06 14:28:30 -0500

    Adding brms model without measurement error

---
+++ R/05b_slope_analysis_brms.R
@@ -0,0 +1,32 @@
+library(brms)
+
+ksm_s <- ksm %>%
+  dplyr::select(mean, se, has_canopy, max_wave_height_estimate, max_temp_slope_estimate, Latitude, Study, Duration) %>%
+  mutate(max_wave_height_estimate = scale(max_wave_height_estimate), 
+         max_temp_slope_estimate = scale(max_temp_slope_estimate), 
+         Latitude = scale(abs(Latitude)),
+         Duration = scale(Duration))
+
+all_factors_brms <- brm(mean | se(se) ~ has_canopy*
+                           max_wave_height_estimate*max_temp_slope_estimate*Latitude +
+                           (1|Study), data=ksm_s)
+
+all_factors_brms
+#plot(marginal_effects(all_factors_brms), ask=TRUE)
+
+a <- posterior_samples(all_factors_brms)
+ctab_brms <- t(apply(a[,1:17], 2, quantile, probs=c(0.05, 0.5, 0.95)))
+ctab_brms <- as.data.frame(ctab_brms)
+ctab_brms$coef_name <- factor(rownames(ctab_brms), levels=rev(rownames(ctab_brms)))
+
+ggplot(data = ctab_brms) +
+  geom_pointrange(mapping=aes(x=coef_name,
+                              y = `50%`,
+                              ymin=`5%`,
+                              ymax = `95%`), size=1.2) +
+  geom_hline(yintercept=0, lty=2) +
+  coord_flip()  
+
+library(shinystan)
+
+launch_shiny(all_factors_brms)
