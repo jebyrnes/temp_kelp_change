96a4707058de98288368265a19cd43c90baa3291
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2016-11-14 11:48:11 -0500

    Changed join order

--- R/04_get_taxa.R
+++ R/04_get_taxa.R
@@ -4,7 +4,7 @@ library(purrr)
 library(meowR)
 
 #read in the kelp slopes data
-kelp_slopes <- read_csv("../derived_data/kelp_slopes_with_temp_waves.csv")
+kelp_slopes <- read_csv("../derived_data/kelp_slopes_with_temp_waves.csv") 
 
 #Merge with envt info from kelp raw data
 raw_data_info <- read_csv("../raw_data/raw_data.csv") %>%
@@ -23,9 +23,15 @@ raw_data_info <- read_csv("../raw_data/raw_data.csv") %>%
             n = length(ECOREGION)) %>%
   ungroup()
 
-kelp_slopes_merged <- right_join(raw_data_info, kelp_slopes) %>%
+kelp_slopes_merged <- left_join(kelp_slopes, raw_data_info, by="SiteName") %>%
   #deal with a few dropped study names
-  mutate(Study = ifelse(is.na(Study), gsub("(.*)\\:", "", SiteName), Study))
+  mutate(Study = ifelse(is.na(Study), gsub("(.*)\\:", "", SiteName), Study)) %>%
+  #deal with some lat/long wonkiness form 
+  dplyr::rename(Latitude = Latitude.y, Longitude = Longitude.y) %>%
+  dplyr::select(-Latitude.x, -Longitude.x)
+
+#anti_join(kelp_slopes, raw_data_info) 
+#anti_join(raw_data_info, kelp_slopes) 
 
 #
 taxa <- read_csv("../raw_data/taxa.csv") %>%
@@ -87,5 +93,23 @@ kelp_slopes_merged_canopy$has_canopy[which(kelp_slopes_merged_canopy$Study=="Tro
 kelp_slopes_merged_canopy$has_canopy[grep("South_Africa", kelp_slopes_merged_canopy$Study)] <- "canopy" #Ecklonia maxima
 
 sum(is.na(kelp_slopes_merged_canopy$has_canopy))
+
+#visual check
+# qplot(Longitude, Latitude, color=has_canopy, data=kelp_slopes_merged_canopy) +
+#   geom_hline(yintercept = 40)+
+#   geom_hline(yintercept = 50)+
+#   geom_vline(xintercept = -10)+
+#   geom_vline(xintercept = 0)
+
+#non-tasmanian Austrlian sites problematic
+kelp_slopes_merged_canopy[which(kelp_slopes_merged_canopy$Longitude>100 & 
+                                  kelp_slopes_merged_canopy$Latitude>-40),]$has_canopy <- "no canopy"
+
+#Europe has no canopy - fix that
+kelp_slopes_merged_canopy[which(kelp_slopes_merged_canopy$Longitude > -10 & 
+                                  kelp_slopes_merged_canopy$Longitude < 0 & 
+                                  kelp_slopes_merged_canopy$Latitude > 40 & 
+                                  kelp_slopes_merged_canopy$Latitude < 50),]$has_canopy <- "no canopy"
+
 
 write_csv(kelp_slopes_merged_canopy, "../derived_data/kelp_slopes_with_temp_waves_canopy.csv")
