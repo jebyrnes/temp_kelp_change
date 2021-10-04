f5fc9c4003f390a3bfbb1399c0f26c1a14efca56
jebyrnes <jarrett.byrnes@umb.edu>
2021-10-04 16:28:48 -0400

    update models

--- scripts/06_model.R
+++ scripts/06_model.R
@@ -18,8 +18,7 @@ mod_regional <- brm(
     mean_temp_province +
     mean_temp_realm +
     Year +
-    (mean_temp_dev * mean_wave_energy_dev |
-       SiteName),
+    (1 | SiteName),
   family = gaussian(link = "log"),
   data = dat,
   iter = 4000,
@@ -29,10 +28,28 @@ mod_regional <- brm(
   save_model = "chain_output/fit_regional_model.Rdata"
 )
 
+
+#squared
+mod_regional <- brm(
+  stdByECOREGION + 0.01 ~
+    ECOREGION * (poly(mean_temp_dev,2) * mean_wave_energy_dev) +
+    mean_wave_energy_site +
+    mean_temp_site +
+    mean_temp_province +
+    mean_temp_realm +
+    Year +
+    (1 |SiteName),
+  family = gaussian(link = "log"),
+  data = dat,
+  iter = 4000,
+  chains = 2,
+  backend = "cmdstanr",
+  threads = threading(4),
+  save_model = "chain_output/fit_regional_model.Rdata"
+)
 # Model with no variation by ecoregion
 #linear
-mod_canopy_linear <- brm(
-  ln_stdByECOREGION + 0.01 ~
+mod_canopy_linear <- brm(bf( ln_stdByECOREGION + 0.01 ~
     (has_canopy * mean_temp_dev * mean_wave_energy_dev) +
     mean_wave_energy_site +
     mean_temp_site +
@@ -41,6 +58,7 @@ mod_canopy_linear <- brm(
     Year +
     (mean_temp_dev * mean_wave_energy_dev|ECOREGION) +
     (1 | SiteName),
+  sigma ~ focalUnit),
   data = dat,
   chains = 2,
   backend = "cmdstanr",
